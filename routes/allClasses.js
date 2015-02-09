fdaa72bf7b83e7a34982f4c4b9b5c21ecd1e247f
Jesus Villegas <jesusvillegas3@gmail.com>
2015-02-08 19:29:46 -0800

    Added first course, not finished with course.js

--- routes/allClasses.js
+++ routes/allClasses.js
@@ -1,33 +1,5 @@
-  
+var courses = require('../courses.json');
 
   exports.view = function(req, res) {
-  	  res.render('allClasses', {
-
-    'courses': [
-      { 'name': 'CSETEST11',
-
-      },
-      { 'name': 'CSETEST12',
-
-      },
-      { 'name': 'CSETEST13',
-
-      },
-      { 'name': 'CSETEST14',
-
-      },
-      { 'name': 'CSETEST15',
-
-      },
-      { 'name': 'CSETEST16',
-
-      },
-      { 'name': 'CSETEST17',
-
-      },
-      { 'name': 'CSETEST18',
-
-      }
-    ]
-  });
+  	  res.render('allClasses', courses);
   };
