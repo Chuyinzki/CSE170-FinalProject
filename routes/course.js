9e698d2f8033b24e30b191b794e3b8184a9b9235
Chuyinzki <jesusvillegas3@gmail.com>
2015-02-12 09:31:14 -0800

    Added View Cape option

--- routes/course.js
+++ routes/course.js
@@ -1,15 +1,20 @@
 var data = require('../courses.json');
 
 	exports.viewCourse = function(req, res) {
-  	  // controller code goes here 
-	  var index = data.index[req.params.name];
-    var course = data.courses[index];
-    console.log("The course index is: " + index);
-    console.log("The course name is: " + course.courseID );
+  	  // controller code goes here
+  	    var index = data.index[req.params.name];
+        var course = data.courses[index];
+        var splitName = course.courseID.split(" ");
+
+        console.log("The course index is: " + index);
+        console.log("The course first name is: " + splitName[0]);
+        console.log("The course second name is: " + splitName[1]);
   	  res.render('course', {
   	  	'courseID': course.courseID,
   	  	'courseName': course.courseName,
-        'courseDescription': course.courseDescription
+        'courseDescription': course.courseDescription,
+        'courseFirstName': splitName[0],
+        'courseSecondName': splitName[1]
   	  });
   	  //console.log("The course name is: ");
   };
