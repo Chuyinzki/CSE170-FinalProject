6a895015567f942a066eaac03af0db01935d97c3
vagrant <vagrant@precise32.(none)>
2015-02-13 07:53:07 +0000

    final push for week6

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -11,3 +11,18 @@ $(document).ready(function() {
 function initializePage() {
 	// add any functionality and listeners you want here
 }
+
+$('#courses-navi').click(function(e)
+{
+	window.location.href = "/courses";
+})
+
+$('#about-navi').click(function(e)
+{
+	window.location.href = "/about";
+})
+
+$('#minor-navi').click(function(e)
+{
+	window.location.href = "/minor";
+})
